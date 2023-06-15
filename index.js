100b4f58cf0ec13c4f44cac6484f3f715735e1fe
Roo Wiki <est.andres.pineros@unimilitar.edu.co>
2023-06-14 23:26:09 -0400

    CV1
                                       .,:cllc:,.
                                   .lONMMMMMMMMMWXk:.
                                 .OMMMMMMMMMMMMWkkKWWx.
                                .XMMMMMMMMMMMMMN0KNWMMN;
                                dMMMMMMMMMMMMMMMMMMMMMMW;
                                XMMMWNWMMMMMMMMMNXWMMMMMX
                               .MMNkxx0XMMMM0oc:lx0NMMMMMc
                               .MX.',..kMMMc  ,,. ,NMMMMMx
                               .WO,WOk.:WNX  OMKO. dMMMMMd
                                XN'0WK;:lco,.OMMN' xMMMMMd
                                0MO'c'''..''..;:'.:WMMMMMX
                                xXl'............,,'KMMMMMMc
                                lXl,.........,,,'':NMMKKNMN.
                                :Mdll;,,,,,,,',,'''kMMXxd0MO
                                OMc.;lc:;;;;;,.    .0MMMNWMMO.
                              .OMX.  .;::;,.         kMMMMMMMX'
                             cNMk.     ...            KMMMMMMMWl
                           .0MMx                      .NMMMMMMMMK.
                          oWMMK'                       ;WMMMMMMMMWc
                         kMMMNo;.      .        ..',;;,.lMMWWMMMMMM0.
                        kMWMWl.                      .';.oMMNXNMMMMMK.
                       xMNNMl                           ..oMMWNWWMMMMX.
                      :MNNWc                               kWXNWKWMMMMX.
                     .NNNW:                                ,MMMMWKMMMMMO
                    .XMWMO                                  XMMMMNMMMMMMc
                   .XMXWM;           '                      kMMMMWMMMMMMX
                  .NMMKNN            '                      xMMMXXMMMMMMM'
                  xMMMNKX           .'                      kMMWKWMMMMMMM'
                  'xc:o0K.          .'                      KWNNWWNNXKWMO
                 .......c0d.         .                  ....lMMMMMMMMX0d.
                '........'kW0;                          '..'dMMMMMMMWx...
          ....,,...........cNMNd.                     .':..'cd0XXXKk:....
        .'..................;KMMM0;                   .;c'..',::::;'......
        .'...................'xWMMMc                  .:l,..................
        .,.....................lX0x'                  oKo;.....................
        .,......................'.                 .cXMKl,......................
        .........................,:'          .':dKMMMMkc'....................
        .........................':kN0kxxkO0KNMMMMMMMMNo:'..............'...
        ..,;;;;,,,'''...........',coNMMMMMMMMMMMMMMMMMKo:,'........',;,'...
          ...',;::cccc;,,'''''',:cox0kddooolllccclodxOOol:;,,,,,;:c:;.....
            ........',;::ccccccll:,..................'';oolllclll:'.....
                ...........',,,'...              ........',,,,'.....

---
+++ index.js
@@ -0,0 +1,282 @@
+import * as THREE from './asd/three.js-master/build/three.module.js'
+import {GLTFLoader} from './asd/three.js-master/examples/jsm/loaders/GLTFLoader.js'
+let giro11, giro22, giro33;
+const canvas = document.querySelector('.webgl')
+const scene = new THREE.Scene()
+
+const render = new THREE.WebGL1Renderer({antialias: true})
+//renderer.shadowMap.enabled = true;
+
+ 
+
+// Importar Objeto con fromato glb
+const loader = new GLTFLoader();
+
+loader.load('assets/logo.glb', function(glb){
+    const root = glb.scene;
+
+    // Posicion y escala del objeto importado
+    root.position.set(0,1.1,0)
+    root.scale.set(0.1, 0.1, 0.1)
+    
+    //Sombras del Objeto Importado
+    root.traverse(function(node){
+        if(node.isMesh)
+            node.castShadow = true;
+            node.receiveShadow = true;
+    });
+
+      scene.add(root);
+
+    
+// Alerta de importacion
+
+}, function(xhr){
+    console.log((xhr.loaded/xhr.total * 100) + "% loaded")
+}, function (error){
+    console.log('An error ocurred')
+})
+
+
+loader.load('assets/giro1.glb', function(glb){
+    const giro1 = glb.scene;
+    giro11 = glb.scene;
+
+    // Posicion y escala del objeto importado
+    //scene.position.set(0,0,0)
+    giro1.position.set(0, 4, 0)
+    giro1.scale.set(0.1, 0.1, 0.1)
+    
+    //Sombras del Objeto Importado
+    giro1.traverse(function(node){
+        if(node.isMesh)
+            node.castShadow = true;
+            node.receiveShadow = true;
+    });
+
+      scene.add(giro1);
+
+})
+loader.load('assets/giro2.glb', function(glb){
+    const giro2 = glb.scene;
+    giro22 = glb.scene;
+
+    // Posicion y escala del objeto importado
+    
+    giro2.position.set(0, 4, 0)
+    giro2.scale.set(0.1, 0.1, 0.1)
+    
+    //Sombras del Objeto Importado
+    giro2.traverse(function(node){
+        if(node.isMesh)
+            node.castShadow = true;
+            node.receiveShadow = true;
+    });
+
+      scene.add(giro2);
+})
+
+loader.load('assets/giro3.glb', function(glb){
+    const giro3 = glb.scene;
+    giro33 = glb.scene;
+
+    // Posicion y escala del objeto importado
+    giro3.position.set(0, 4, 0)
+    giro3.scale.set(0.1, 0.1, 0.1)
+    
+    //Sombras del Objeto Importado
+    giro3.traverse(function(node){
+        if(node.isMesh)
+            node.castShadow = true;
+            node.receiveShadow = true;
+    });
+
+      scene.add(giro3);
+})
+
+let rotationSpeed = 0.03
+
+
+                                                        //__̴ı̴̴̡̡̡ ̡͌l̡̡̡ ̡͌l̡*̡̡ ̴̡ı̴̴̡ ̡̡͡|̲̲̲͡͡͡ ̲▫̲͡ ̲̲̲͡͡π̲̲͡͡ ̲̲͡▫̲̲͡͡ ̲|̡̡̡ ̡ ̴̡ı̴̡̡ ̡͌l̡̡̡̡.___// Iluminacion general de la escena                                     
+const light = new THREE.DirectionalLight(0x8365FF, 1)
+light.position.set(5, 2,5)
+light.castShadow = true
+scene.add(light)   
+
+const light3 = new THREE.DirectionalLight(0xCD30FF, 1)
+light3.position.set(-5, 2,4)
+light3.castShadow = true
+scene.add(light3)
+/*
+const light4 = new THREE.DirectionalLight(0xFAEAFF, 1)
+light4.position.set(-5, 2,-4)
+light4.castShadow = true
+scene.add(light4)
+*/
+const ambientLight = new THREE.AmbientLight(0x3200FF, 0.3);
+scene.add(ambientLight);
+
+
+const ambientLight1 = new THREE.AmbientLight(0xFFFFFF, 0.3);
+scene.add(ambientLight1);
+
+
+const pointLight = new THREE.PointLight(0xE001FF, 5, 5, 1);
+pointLight.position.set(3, 3, 3);
+pointLight.castShadow = true
+scene.add(pointLight);
+
+const pointLight1 = new THREE.PointLight(0x13FF00, 5, 5, 1);
+pointLight1.position.set(3, 3, -3);
+pointLight1.castShadow = true
+scene.add(pointLight1);
+
+/*
+const spotLight = new THREE.SpotLight(0xFF8C20, 10, 0, 0.5, 3, 3);
+spotLight.position.set(0, 5, 0);
+spotLight.target.position.set(0, 0, 0);
+spotLight.castShadow = true
+scene.add(spotLight);
+scene.add(spotLight.target);
+*/
+
+
+// Luces del Arbol
+
+const ArbolPointLight1 = new THREE.PointLight(0x00FF17, 100, 3);
+ArbolPointLight1.position.set(-3, 3, 0);
+ArbolPointLight1.castShadow = true
+scene.add(ArbolPointLight1);
+
+
+
+
+
+
+
+
+                                                        //__̴ı̴̴̡̡̡ ̡͌l̡̡̡ ̡͌l̡*̡̡ ̴̡ı̴̴̡ ̡̡͡|̲̲̲͡͡͡ ̲▫̲͡ ̲̲̲͡͡π̲̲͡͡ ̲̲͡▫̲̲͡͡ ̲|̡̡̡ ̡ ̴̡ı̴̡̡ ̡͌l̡̡̡̡. // Sky Box
+// Crear el skybox con texturas
+const loaderskybox = new THREE.CubeTextureLoader();
+const texture = loaderskybox.load([
+  'assets/SkyBox/1.jpg',
+  'assets/SkyBox/3.jpg',
+  'assets/SkyBox/5.jpg',
+  'assets/SkyBox/6.jpg',
+  'assets/SkyBox/4.jpg',
+  'assets/SkyBox/2.jpg'
+]);
+scene.background = texture;
+
+                                                        //__̴ı̴̴̡̡̡ ̡͌l̡̡̡ ̡͌l̡*̡̡ ̴̡ı̴̴̡ ̡̡͡|̲̲̲͡͡͡ ̲▫̲͡ ̲̲̲͡͡π̲̲͡͡ ̲̲͡▫̲̲͡͡ ̲|̡̡̡ ̡ ̴̡ı̴̡̡ ̡͌l // Tamaño de la escena
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+                                                        //__̴ı̴̴̡̡̡ ̡͌l̡̡̡ ̡͌l̡*̡̡ ̴̡ı̴̴̡ ̡̡͡|̲̲̲͡͡͡ ̲▫̲͡ ̲̲̲͡͡π̲̲͡͡ ̲̲͡▫̲̲͡͡ ̲|̡̡̡ ̡ ̴̡ı̴̡̡ ̡͌l
+// Camara
+const camera = new  THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, 0.1, 1000);
+
+
+const positions = [
+    { x: 5, y: 1.5, z: 5 },
+    { x: -5, y: 1.5, z: 5 },
+    { x: -5, y: 1.5, z: -5 },
+    { x: 5, y: 1.5, z: -5 },
+    { x: 0, y: 1.5, z: -5 }
+  ];
+  
+  let clickCount = 0; // Contador de clics
+  
+  function animateCamera() {
+    if (clickCount < positions.length) {
+      const position = positions[clickCount];
+      new TWEEN.Tween(camera.position)
+        .to(position, 2000) // Duración de la animación en milisegundos
+        .easing(TWEEN.Easing.Quadratic.Out)
+        .onComplete(() => {
+          // Animación completada, incrementar el contador de clics y permitir otro clic
+          clickCount++;
+          console.log('clickCount: ' + clickCount);
+        })
+        .start();
+      console.log('Carga');
+    } else {
+      console.log('Animación completada');
+      clickCount = 0
+      animateCamera();
+    }
+  }
+  
+  function handleClick() {
+    animateCamera();
+  }
+  
+  // Asignar el controlador de eventos al hacer clic en el lienzo
+  canvas.addEventListener('click', handleClick, false);
+
+// Variables para controlar la rotación de la cámara
+
+
+scene.add(camera);
+
+
+//const renderer2 = new THREE.WebGL1Renderer({ antialias: true });  
+const renderer = new THREE.WebGL1Renderer({
+
+    canvas: canvas
+})
+
+//renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+//renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio))
+renderer.setClearColor(0xA3A3A3);
+
+window.addEventListener( 'resize', onWindowResize );
+
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+////////////////////////////////////////////////////// Función de manejo del evento de clic en el lienzo
+
+
+function animate(){
+    // Actualizar los tweens en cada fotograma
+    
+    function onCanvasClick(event) {
+        //createCameraAnimations();
+    }
+
+    
+    TWEEN.update();
+      
+    // Agrega un event listener al elemento del lienzo de renderizado
+    renderer.domElement.addEventListener('click', onCanvasClick);
+    
+
+    if (giro11) giro11.rotation.x += rotationSpeed;
+    if (giro22) giro22.rotation.z += rotationSpeed;
+    if (giro33) giro33.rotation.y += rotationSpeed;
+
+    
+        // Apuntar la camara
+    camera.lookAt(0.0, 1, 0.0);
+
+    renderer.shadowMap.enabled = true
+    renderer.gammaOuput = true
+            
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+    }
+            
+animate()
+
+
