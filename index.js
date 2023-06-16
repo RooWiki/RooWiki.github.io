6d2165279e04b37d4f5c2a7852a8b93f5677f283
Roo Wiki <est.andres.pineros@unimilitar.edu.co>
2023-06-16 16:58:02 -0400

    CV22

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import * as THREE from './asd/three.js-master/build/three.module.js'
-import {GLTFLoader} from './asd/three.js-master/examples/jsm/loaders/GLTFLoader.js'
+import {
+   GLTFLoader
+} from './asd/three.js-master/examples/jsm/loaders/GLTFLoader.js'
 let giro11, giro22, giro33;
 const canvas = document.querySelector('.webgl')
 const scene = new THREE.Scene()
@@ -18,85 +20,85 @@ const button4 = document.getElementById('button4');
 const loader = new GLTFLoader();
 
 
-loader.load('assets/logo.glb', function(glb){
-    const root = glb.scene;
+loader.load('assets/logo.glb', function (glb) {
+   const root = glb.scene;
 
-    // Posicion y escala del objeto importado
-    root.position.set(0,1.1,0)
-    root.scale.set(0.1, 0.1, 0.1)
-    
-    //Sombras del Objeto Importado
-    root.traverse(function(node){
-        if(node.isMesh)
-            node.castShadow = true;
-            node.receiveShadow = true;
-    });
+   // Posicion y escala del objeto importado
+   root.position.set(0, 1.1, 0)
+   root.scale.set(0.1, 0.1, 0.1)
 
-      scene.add(root);
+   //Sombras del Objeto Importado
+   root.traverse(function (node) {
+      if (node.isMesh)
+         node.castShadow = true;
+      node.receiveShadow = true;
+   });
 
-    
-// Alerta de importacion
+   scene.add(root);
 
-}, function(xhr){
-    console.log((xhr.loaded/xhr.total * 100) + "% loaded")
-}, function (error){
-    console.log('An error ocurred')
+
+   // Alerta de importacion
+
+}, function (xhr) {
+   console.log((xhr.loaded / xhr.total * 100) + "% loaded")
+}, function (error) {
+   console.log('An error ocurred')
 })
 
 
-loader.load('assets/giro1.glb', function(glb){
-    const giro1 = glb.scene;
-    giro11 = glb.scene;
+loader.load('assets/giro1.glb', function (glb) {
+   const giro1 = glb.scene;
+   giro11 = glb.scene;
 
-    giro1.position.set(0, 4, 0)
-    giro1.scale.set(0.1, 0.1, 0.1)
-    
-    //Sombras del Objeto Importado
-    giro1.traverse(function(node){
-        if(node.isMesh)
-            node.castShadow = true;
-            node.receiveShadow = true;
-    });
+   giro1.position.set(0, 4, 0)
+   giro1.scale.set(0.1, 0.1, 0.1)
 
-      scene.add(giro1);
+   //Sombras del Objeto Importado
+   giro1.traverse(function (node) {
+      if (node.isMesh)
+         node.castShadow = true;
+      node.receiveShadow = true;
+   });
+
+   scene.add(giro1);
 
 })
-loader.load('assets/giro2.glb', function(glb){
-    const giro2 = glb.scene;
-    giro22 = glb.scene;
-    
-    giro2.position.set(0, 4, 0)
-    giro2.scale.set(0.1, 0.1, 0.1)
-    
-    giro2.traverse(function(node){
-        if(node.isMesh)
-            node.castShadow = true;
-            node.receiveShadow = true;
-    });
+loader.load('assets/giro2.glb', function (glb) {
+   const giro2 = glb.scene;
+   giro22 = glb.scene;
 
-      scene.add(giro2);
+   giro2.position.set(0, 4, 0)
+   giro2.scale.set(0.1, 0.1, 0.1)
+
+   giro2.traverse(function (node) {
+      if (node.isMesh)
+         node.castShadow = true;
+      node.receiveShadow = true;
+   });
+
+   scene.add(giro2);
 })
 
-loader.load('assets/giro3.glb', function(glb){
-    const giro3 = glb.scene;
-    giro33 = glb.scene;
+loader.load('assets/giro3.glb', function (glb) {
+   const giro3 = glb.scene;
+   giro33 = glb.scene;
 
-    giro3.position.set(0, 4, 0)
-    giro3.scale.set(0.1, 0.1, 0.1)
-    
-    giro3.traverse(function(node){
-        if(node.isMesh)
-            node.castShadow = true;
-            node.receiveShadow = true;
-    });
+   giro3.position.set(0, 4, 0)
+   giro3.scale.set(0.1, 0.1, 0.1)
 
-      scene.add(giro3);
+   giro3.traverse(function (node) {
+      if (node.isMesh)
+         node.castShadow = true;
+      node.receiveShadow = true;
+   });
+
+   scene.add(giro3);
 })
 let rotationSpeed = 0.03
 
 
 // Iluminacion general de la escena
-                                    
+
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight)
 
@@ -140,13 +142,13 @@ scene.add(Farol2);
 // verde 
 const Esmeraldas1 = new THREE.PointLight(0x1FFF00, 50, 2);
 Esmeraldas1.position.set(-3, 2.5, -3.5);
-Esmeraldas1.castShadow = false 
+Esmeraldas1.castShadow = false
 scene.add(Esmeraldas1);
 // verde 
 
 // Puerta principal
 // Azul 
-const Puerta = new THREE.PointLight(0x00C5FF, 10, 1);
+const Puerta = new THREE.PointLight(0x00C5FF, 30, 1);
 Puerta.position.set(0, 1, 1.5);
 Puerta.castShadow = false
 scene.add(Puerta);
@@ -186,96 +188,115 @@ luna.castShadow = false
 scene.add(luna);
 
 
-                                                        //__̴ı̴̴̡̡̡ ̡͌l̡̡̡ ̡͌l̡*̡̡ ̴̡ı̴̴̡ ̡̡͡|̲̲̲͡͡͡ ̲▫̲͡ ̲̲̲͡͡π̲̲͡͡ ̲̲͡▫̲̲͡͡ ̲|̡̡̡ ̡ ̴̡ı̴̡̡ ̡͌l̡̡̡̡. // Sky Box
+//__̴ı̴̴̡̡̡ ̡͌l̡̡̡ ̡͌l̡*̡̡ ̴̡ı̴̴̡ ̡̡͡|̲̲̲͡͡͡ ̲▫̲͡ ̲̲̲͡͡π̲̲͡͡ ̲̲͡▫̲̲͡͡ ̲|̡̡̡ ̡ ̴̡ı̴̡̡ ̡͌l̡̡̡̡. // Sky Box
 // Crear el skybox con texturas
 const loaderskybox = new THREE.CubeTextureLoader();
 const texture = loaderskybox.load([
-  'assets/SkyBox/1.jpg',
-  'assets/SkyBox/3.jpg',
-  'assets/SkyBox/5.jpg',
-  'assets/SkyBox/6.jpg',
-  'assets/SkyBox/4.jpg',
-  'assets/SkyBox/2.jpg'
+   'assets/SkyBox/1.jpg',
+   'assets/SkyBox/3.jpg',
+   'assets/SkyBox/5.jpg',
+   'assets/SkyBox/6.jpg',
+   'assets/SkyBox/4.jpg',
+   'assets/SkyBox/2.jpg'
 ]);
 scene.background = texture;
 
-                                                        //__̴ı̴̴̡̡̡ ̡͌l̡̡̡ ̡͌l̡*̡̡ ̴̡ı̴̴̡ ̡̡͡|̲̲̲͡͡͡ ̲▫̲͡ ̲̲̲͡͡π̲̲͡͡ ̲̲͡▫̲̲͡͡ ̲|̡̡̡ ̡ ̴̡ı̴̡̡ ̡͌l // Tamaño de la escena
+//__̴ı̴̴̡̡̡ ̡͌l̡̡̡ ̡͌l̡*̡̡ ̴̡ı̴̴̡ ̡̡͡|̲̲̲͡͡͡ ̲▫̲͡ ̲̲̲͡͡π̲̲͡͡ ̲̲͡▫̲̲͡͡ ̲|̡̡̡ ̡ ̴̡ı̴̡̡ ̡͌l // Tamaño de la escena
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+   width: window.innerWidth,
+   height: window.innerHeight
 }
 
-                                                        //__̴ı̴̴̡̡̡ ̡͌l̡̡̡ ̡͌l̡*̡̡ ̴̡ı̴̴̡ ̡̡͡|̲̲̲͡͡͡ ̲▫̲͡ ̲̲̲͡͡π̲̲͡͡ ̲̲͡▫̲̲͡͡ ̲|̡̡̡ ̡ ̴̡ı̴̡̡ ̡͌l
+//__̴ı̴̴̡̡̡ ̡͌l̡̡̡ ̡͌l̡*̡̡ ̴̡ı̴̴̡ ̡̡͡|̲̲̲͡͡͡ ̲▫̲͡ ̲̲̲͡͡π̲̲͡͡ ̲̲͡▫̲̲͡͡ ̲|̡̡̡ ̡ ̴̡ı̴̡̡ ̡͌l
 // Camara
-const camera = new  THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, 0.1, 1000);
-camera.position.set(2,2.5,7)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(2, 2.5, 7)
 
-const positions = [
-    { x: 2, y: 2.5, z: 7 },
-    { x: 0, y: 1.5, z: 5 },
-    { x: -5, y: 1, z: -1.5 },
-    { x: 0, y: 1, z: -3 },
-    { x: 5, y: 1, z: 0 }
-  ];
-  
-  let clickCount = 0; // Contador de clics
+const positions = [{
+      x: 2,
+      y: 2.5,
+      z: 7
+   },
+   {
+      x: 0,
+      y: 1.5,
+      z: 5
+   },
+   {
+      x: -5,
+      y: 1,
+      z: -1.5
+   },
+   {
+      x: 0,
+      y: 1,
+      z: -3
+   },
+   {
+      x: 5,
+      y: 1,
+      z: 0
+   }
+];
 
-  logoCV.addEventListener('click', () => {
-    clickCount = 0;
-    console.log('Se ha hecho clic en el botón 0. clickCount =', clickCount);
-    animateCamera() 
-  });
+let clickCount = 0; // Contador de clics
 
-  button1.addEventListener('click', () => {
-    clickCount = 1;
-    console.log('Se ha hecho clic en el botón 1. clickCount =', clickCount);
-    animateCamera() 
-  });
+logoCV.addEventListener('click', () => {
+   clickCount = 0;
+   console.log('Se ha hecho clic en el botón 0. clickCount =', clickCount);
+   animateCamera()
+});
 
-  button2.addEventListener('click', () => {
-    clickCount = 2;
-    console.log('Se ha hecho clic en el botón 2. clickCount =', clickCount);
-    animateCamera() 
-  });
+button1.addEventListener('click', () => {
+   clickCount = 1;
+   console.log('Se ha hecho clic en el botón 1. clickCount =', clickCount);
+   animateCamera()
+});
 
-  button3.addEventListener('click', () => {
-    clickCount = 3;
-    console.log('Se ha hecho clic en el botón 3. clickCount =', clickCount);
-    animateCamera() 
-  });
+button2.addEventListener('click', () => {
+   clickCount = 2;
+   console.log('Se ha hecho clic en el botón 2. clickCount =', clickCount);
+   animateCamera()
+});
 
-  button4.addEventListener('click', () => {
-    clickCount = 4;
-    console.log('Se ha hecho clic en el botón 4. clickCount =', clickCount);
-    animateCamera() 
-  });
-  
-  function animateCamera() {
-    if (clickCount < positions.length) {
+button3.addEventListener('click', () => {
+   clickCount = 3;
+   console.log('Se ha hecho clic en el botón 3. clickCount =', clickCount);
+   animateCamera()
+});
+
+button4.addEventListener('click', () => {
+   clickCount = 4;
+   console.log('Se ha hecho clic en el botón 4. clickCount =', clickCount);
+   animateCamera()
+});
+
+function animateCamera() {
+   if (clickCount < positions.length) {
       const position = positions[clickCount];
       new TWEEN.Tween(camera.position)
-        .to(position, 2000) // Duración de la animación en milisegundos
-        .easing(TWEEN.Easing.Quadratic.Out)
-        .onComplete(() => {
-          // Animación completada, incrementar el contador de clics y permitir otro clic
-          clickCount++;
-          console.log('clickCount: ' + clickCount);
-        })
-        .start();
+         .to(position, 2000) // Duración de la animación en milisegundos
+         .easing(TWEEN.Easing.Quadratic.Out)
+         .onComplete(() => {
+            // Animación completada, incrementar el contador de clics y permitir otro clic
+            clickCount++;
+            console.log('clickCount: ' + clickCount);
+         })
+         .start();
       console.log('Carga');
-    } else {
+   } else {
       console.log('Animación completada');
       clickCount = 0
       animateCamera();
-    }
-  }
-  /* Si le das click a la escena, se mueve la camara
-  function handleClick() {
-    animateCamera();
-  }*/
-  
-  // Asignar el controlador de eventos al hacer clic en el lienzo
-  //canvas.addEventListener('click', handleClick, false);
+   }
+}
+/* Si le das click a la escena, se mueve la camara
+function handleClick() {
+  animateCamera();
+}*/
+
+// Asignar el controlador de eventos al hacer clic en el lienzo
+//canvas.addEventListener('click', handleClick, false);
 
 // Variables para controlar la rotación de la cámara
 
@@ -285,7 +306,7 @@ scene.add(camera);
 
 const renderer = new THREE.WebGL1Renderer({
 
-    canvas: canvas
+   canvas: canvas
 })
 
 //renderer.shadowMap.enabled = true;
@@ -296,13 +317,13 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio))
 renderer.setClearColor(0xA3A3A3);
 
-window.addEventListener( 'resize', onWindowResize );
+window.addEventListener('resize', onWindowResize);
 
 
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+   camera.aspect = window.innerWidth / window.innerHeight;
+   camera.updateProjectionMatrix();
+   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 /////////////////////////////////////////////////////
@@ -406,37 +427,34 @@ window.addEventListener('keyup', onKeyUp, false);
 
 */
 ////////////////////////////////////////////////////// Función de manejo del evento de clic en el lienzo
-rotationSpeed 
+rotationSpeed
 
-function animate(){
-    // Actualizar los tweens en cada fotograma
-    function onCanvasClick(event) {
-        //createCameraAnimations();
-    }
-    TWEEN.update();
-      
-    // Agrega un event listener al elemento del lienzo de renderizado
-    //cameraTheta += rotationSpeedX;
-    //cameraPhi += rotationSpeedY;
-    //updateCameraPosition();
-  
+function animate() {
+   // Actualizar los tweens en cada fotograma
+   function onCanvasClick(event) {
+      //createCameraAnimations();
+   }
+   TWEEN.update();
 
-    if (giro11) giro11.rotation.x += rotationSpeed;
-    if (giro22) giro22.rotation.z += rotationSpeed;
-    if (giro33) giro33.rotation.y += rotationSpeed;
-    
-    
-    
-        // Apuntar la camara
-    camera.lookAt(0.0, 1, 0.0);
+   // Agrega un event listener al elemento del lienzo de renderizado
+   //cameraTheta += rotationSpeedX;
+   //cameraPhi += rotationSpeedY;
+   //updateCameraPosition();
 
-    renderer.shadowMap.enabled = true
-    renderer.gammaOuput = true
-    renderer.domElement.addEventListener('click', onCanvasClick);
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-    }
+
+   if (giro11) giro11.rotation.x += rotationSpeed;
+   if (giro22) giro22.rotation.z += rotationSpeed;
+   if (giro33) giro33.rotation.y += rotationSpeed;
+
+
+   // Apuntar la camara
+   camera.lookAt(0.0, 1, 0.0);
+
+   renderer.shadowMap.enabled = true
+   renderer.gammaOuput = true
+   renderer.domElement.addEventListener('click', onCanvasClick);
+   requestAnimationFrame(animate);
+   renderer.render(scene, camera);
+}
 //updateCameraPosition();
 animate()
-
-
